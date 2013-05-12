a58f648e26ae7a5b255cef89721fe99b737f386c
Luca Ongaro <lukeongaro@gmail.com>
2013-05-13 01:02:34 +0200

    add views

--- lib/share/app.rb
+++ lib/share/app.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require "sinatra/activerecord"
 require "sinatra/contrib"
+require "erb"
 require "./lib/share/channel"
 require "./lib/share/message"
 
@@ -9,6 +10,26 @@ module Share
     register Sinatra::ActiveRecordExtension
     register Sinatra::Contrib
     set :database, "sqlite3:///share.sqlite3"
+    set :root, "./"
+
+    # test
+    post "/test" do
+      logger.info params.inspect
+      "Received POST with params: #{params.inspect}"
+    end
+    get "/test" do
+      logger.info params.inspect
+      "Received GET with params: #{params.inspect}"
+    end
+
+    # home
+    get "/" do
+      if params[:slug].nil?
+        respond_with :home
+      else
+        redirect to("/#{params[:slug]}")
+      end
+    end
 
     # show channel
     get "/:slug" do
@@ -19,7 +40,9 @@ module Share
     # create new channel
     post "/" do
       channel = Channel.create
-      respond_with :index, channels: Channel.all
+      respond_with :show, channel: channel do |type|
+        type.html { redirect to("/#{channel.slug}") }
+      end
     end
 
     # create new message in channel
@@ -29,16 +52,5 @@ module Share
       respond_with :show, channel: channel
     end
 
-    # test
-
-    post "/test" do
-      logger.info params.inspect
-      params.inspect
-    end
-
-    get "/test" do
-      logger.info params.inspect
-      params.inspect
-    end
   end
 end
--- lib/share/message.rb
+++ lib/share/message.rb
@@ -1,5 +1,10 @@
 module Share
   class Message < ActiveRecord::Base
     belongs_to :channel
+
+    def author
+      # TODO: this is a stub
+      "Anonymous"
+    end
   end
 end
