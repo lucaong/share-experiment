bd9f1469cef32ac3766d68af0b47ff84531f23cd
Luca Ongaro <lukeongaro@gmail.com>
2013-05-20 14:01:20 +0200

    escape html and produce markup for URLs, Twitter usernames and emails

--- lib/share/message.rb
+++ lib/share/message.rb
@@ -19,14 +19,32 @@ module Share
     private
 
     def markup_body
-      self.body = Rack::Utils.escape_html(body)
+      # Escape html (but not slashes)
+      body.gsub! /&|<|>|'|"/ do |c|
+        escaped = {
+          "&"=>"&amp;",
+          "<"=>"&lt;",
+          ">"=>"&gt;",
+          "'"=>"&#x27;"
+        }
+        escaped[c]
+      end
 
-      # Twitter handles
+      # Decorate URLs. RegExp pattern from:
+      # http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+      url_regexp = /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/
+      body.gsub! url_regexp do |orig|
+        url = orig
+        url = "http://#{url}" unless url.match /^[a-z][\w-]+:/
+        "<a href='#{url}' target='_blank'>#{orig}</a>"
+      end
+
+      # Decorate Twitter handles
       body.gsub! /(^|\s)@(\w+)/ do
         "#{$1}<a href='https://twitter.com/#{$2}' target='_blank'>@#{$2}</a>"
       end
 
-      # email addresses
+      # Decorate email addresses
       body.gsub! /(\b)([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})(\b)/i do
         "#{$1}<a href='mailto:#{$2}'>#{$2}</a>#{$3}"
       end
